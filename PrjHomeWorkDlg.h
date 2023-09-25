0e5676255d90757939d5e2c60ddb2f14a10a54b8
ToryHwang <tory45@empal.com>
2023-09-25 14:28:55 +0900

    Delete .suo

---
+++ PrjHomeWorkDlg.h
@@ -0,0 +1,52 @@
+﻿
+// PrjHomeWorkDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CPrjHomeWorkDlg 대화 상자
+class CPrjHomeWorkDlg : public CDialogEx
+{
+
+// User Function
+protected:
+
+private:
+	CImage m_image;
+	int m_nImagePosY = 0;
+
+	void InitDlg();
+	void UpdateDisplay();
+	BOOL ReleaseDlg();
+public:
+
+// 생성입니다.
+public:
+	CPrjHomeWorkDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_PRJHOMEWORK_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	int m_Ctl_nRadius;
+	int m_Ctl_nMakeCnt;
+	afx_msg void OnBnClickedBtnClose();
+	afx_msg void OnBnClickedBtnMake();
+};
---
+++ UserDef.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define IMG_WIDTH	640
+#define IMG_HEIGHT	480
+#define IMG_BIT		8
+
+#define MARK_LEN	3
